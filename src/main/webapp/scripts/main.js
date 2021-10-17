bfddfbb3d8e7c4f6651c7c4e9623423963d615f2
hoangnhat42 <hoangnhat.contact@gmail.com>
2021-10-17 15:53:24 +0700

    fix giao dien lan nua :V

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -85,3 +85,16 @@ function setupSelector(selector) {
     }
   });
 }
+
+/*Desgin Button Require */
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
+
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
+});
